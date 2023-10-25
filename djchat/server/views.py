c1a9ab0e536f8d4840a2612dba14ab0c450ae95c
Chukmarov <chukmarov.roman@yandex.ru>
2023-10-25 22:53:31 +0500

    Сделал фильтр Серверов по кол-ву (просто обрезка), также сделал фильтрацию по серверам, которые принадлежат только этому пользователю, а также зарегистрировал Users в админке, чтоб можно было удалять , добавлять и проводить манипуляии с пользователями

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -20,6 +20,15 @@ class ServerListViewSet(viewsets.ViewSet):
         # и писваиваем его переменной
         category = request.query_params.get("category")
 
+        # Находим параметр qty в запросе и
+        # присваиваем его переменной
+        qty = request.query_params.get("qty")
+
+        # Этот параметр мы также ловим в запросе.
+        # Если он присутвует и равняется true,
+        # то присваиваем его переменной
+        by_user = request.query_params.get("by_user") == "true"
+
         if category:
             # Если категория есть, то выполняется последующий код,
             # если категории нет в запросе, то вернутся
@@ -31,6 +40,23 @@ class ServerListViewSet(viewsets.ViewSet):
             # ищутся по ID номеру
 
             self.queryset = self.queryset.filter(category__name=category)
+
+        if by_user:
+            # Если в запросе присутвует параметр by_user.
+            # То мы находим Id пользователя и присваиваем
+            # его переменой, после чего фильтруем ранее
+            # полученные данные. Важно что он должен стоять
+            # и именно перед урезанием массива , т.е.
+            # перед if qty. Иначе сервер начинает ругаться.
+            user_id = request.user.id
+            self.queryset = self.queryset.filter(member=user_id)
+
+        if qty:
+            # Если в запросе указано кол-во, то мы просто
+            # отфильтровываем выдачу согласно заданному кол-ву.
+            # Простая нарезка списков/сетов
+
+            self.queryset = self.queryset[: int(qty)]
 
         # Вот тут в сериалайзере присутвует приписка many.
         # Как я понял эта приписка обязательна если в подели
