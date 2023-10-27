05839dac6e5e007641d8073143c04202da8e3cf4
Chukmarov <chukmarov.roman@yandex.ru>
2023-10-27 22:20:01 +0500

    Связал два сериалайзера вместе Channel и Server, для того чтоб одним запросом выгружать информацию по обоим объектам

--- djchat/server/serializer.py
+++ djchat/server/serializer.py
@@ -1,11 +1,26 @@
 from rest_framework import serializers
 
-from .models import Category, Server
+from .models import Category, Server, Channel
+
+
+class ChannelSerialaizer(serializers.ModelSerializer):
+    # Этот сериалайзер возвращает все даные модели Channel
+    class Meta:
+        model = Channel
+        fields = "__all__"
 
 
 class ServerSerializer(serializers.ModelSerializer):
+    # Этот параметр есть в модели Server,
+    # просто мы подсказываем сериалайзеру
+    # на какой другой сериалайзер ему ссылаться
+    # Также нужно обратить внимание на параметр
+    # many= True, без него не будут
+    # прогружаться все каналы, да и вообще
+    # все перестанет работать.
+    channel_server = ChannelSerialaizer(many=True)
+
     # Этот сериалайзер возвразщает все данные модели Server
-    # Ну в целом это из кода даже видно
     class Meta:
         model = Server
         fields = "__all__"
