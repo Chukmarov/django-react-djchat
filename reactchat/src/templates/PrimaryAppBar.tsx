4182ed3bcd619d27e708693dd1868b3ecc130def
Roman Chukmarov <romanchukmarov@MacBook-Pro-Roman.local>
2023-11-11 12:59:39 +0500

    Добавил желаемые шрифты в тему, а также прописал адаптивность шрифтов

--- reactchat/src/templates/PrimaryAppBar.tsx
+++ reactchat/src/templates/PrimaryAppBar.tsx
@@ -1,44 +1,47 @@
-import {AppBar, Toolbar, Link, Typography} from "@mui/material";
-import {useTheme} from "@mui/material/styles"; // импортировать это
+import { AppBar, Toolbar, Link, Typography } from "@mui/material";
+import { useTheme } from "@mui/material/styles"; // импортировать это
 
 const PrimaryAppBar = () => {
-    const theme = useTheme()
-    return (
-        // в toolbar мы импортируем тему, которую ранее создали
-        <AppBar
-            sx={{
-                /* Ниже мы сбрасываем цвет верхнего меню до белого цвета, а
+  const theme = useTheme();
+  return (
+    // в toolbar мы импортируем тему, которую ранее создали
+    <AppBar
+      sx={{
+        /* Ниже мы сбрасываем цвет верхнего меню до белого цвета, а
             также настраиваем цвет нижней границы */
-                backgroundColor: theme.palette.background.default,
-                borderBottom: `1px solid ${theme.palette.divider}`
-            }}>
-            <Toolbar
-                variant="dense"
-                sx={{
-                    height: theme.primaryAppBar.height,
-                    minHeight: theme.primaryAppBar.height
-                }}>
-                {/* 
+        backgroundColor: theme.palette.background.default,
+        borderBottom: `1px solid ${theme.palette.divider}`,
+      }}
+    >
+      <Toolbar
+        variant="dense"
+        sx={{
+          height: theme.primaryAppBar.height,
+          minHeight: theme.primaryAppBar.height,
+        }}
+      >
+        {/* 
                 Ниже мы настраиваем логотип как ссылку саму на себя
                 Также прописываем размер шрифта, не забудьте импортировать
                 компоненты Link и Typography
                 */}
-                <Link href="/" underline="none" color="inherit">
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="div"
-                        sx={{
-                            display: {
-                                fontWeight: 700,
-                                letterSpacing: "-0,5px"
-                            }
-                        }}>
-                        DJCHAT
-                    </Typography>
-                </Link>
-            </Toolbar>
-        </AppBar>
-    );
+        <Link href="/" underline="none" color="inherit">
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{
+              display: {
+                fontWeight: 700,
+                letterSpacing: "-0,5px",
+              },
+            }}
+          >
+            DJCHAT
+          </Typography>
+        </Link>
+      </Toolbar>
+    </AppBar>
+  );
 };
 export default PrimaryAppBar;
--- reactchat/src/theme/theme.tsx
+++ reactchat/src/theme/theme.tsx
@@ -1,4 +1,4 @@
-import { createTheme } from "@mui/material";
+import { createTheme, responsiveFontSizes } from "@mui/material";
 
 /*
 Этот блок declare module используется для 
@@ -6,52 +6,65 @@ import { createTheme } from "@mui/material";
 TypeScript о том, что добавляем новые свойства 
 в объект типа Theme и ThemeOptions 
 из библиотеки Material-UI.
-*/  
+*/
 declare module "@mui/material/styles" {
-
-    /*
+  /*
     Далее мы расширяем тип Theme, 
     добавляя новое свойство primaryAppBar, 
     которое представляет собой объект 
     с единственным свойством height, 
     представляющим высоту панели приложения.
-    */ 
-    interface Theme {
-        primaryAppBar: {
-            height : number;
-        };
-    }
+    */
+  interface Theme {
+    primaryAppBar: {
+      height: number;
+    };
+  }
 
-    interface ThemeOptions {
-        primaryAppBar: {
-            height: number;
-        };
-    }
+  interface ThemeOptions {
+    primaryAppBar: {
+      height: number;
+    };
+  }
 }
 
- /* Теперь, когда мы добавили новый параметр 
- в Интерфейс, мы перезапишем функцию 
- создания темы , где укажем размер верхнего меню */
-export const createMuiTheme = () =>{
-    let theme = createTheme({
-        primaryAppBar:{
-            height:50,
-        },
-        /* 
+/* 
+Теперь, когда мы добавили новый параметр 
+в Интерфейс, мы перезапишем функцию 
+создания темы , где укажем размер верхнего меню */
+export const createMuiTheme = () => {
+  let theme = createTheme({
+    /* 
+    тут понятно что мы прописываем настройку шрифта
+    Но по сути , если мы берем roboto, то у нас ничего не 
+    моеняется. проект и так по умолчанию roboto
+    */
+    typography: {
+      fontFamily: ["Roboto", "sans-serif"].join(","),
+    },
+    primaryAppBar: {
+      height: 50,
+    },
+    /* 
         ниже мы обнуляем цвет текста в верхнем
         меню на по-умолчанию, т.е. на черный, а также
         убираем тень верхнего меню
         */
-        components:{
-            MuiAppBar:{
-                defaultProps:{
-                    color:"default",
-                    elevation:0,
-                }
-            }
-        }
-    })
-    return theme;
+    components: {
+      MuiAppBar: {
+        defaultProps: {
+          color: "default",
+          elevation: 0,
+        },
+      },
+    },
+  });
+  /* 
+  Выше мы импортировали настройки адаптивных шрифтов
+  А тут мы пропускаем нашу тему через эту настройку
+  */
+  theme = responsiveFontSizes(theme);
+  return theme;
 };
 
 /* Ну и наконец экспортируем эту функцию */
