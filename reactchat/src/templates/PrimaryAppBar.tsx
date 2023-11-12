8df356feb2b78774e0db4a9af60f31d08bd7b2ff
Roman Chukmarov <romanchukmarov@MacBook-Pro-Roman.local>
2023-11-12 23:33:52 +0500

    добавил скрытие бокового меню в зависимости от размеров экрана

--- reactchat/src/templates/PrimaryAppBar.tsx
+++ reactchat/src/templates/PrimaryAppBar.tsx
@@ -1,14 +1,55 @@
-import { AppBar, Toolbar, Link, Typography } from "@mui/material";
-import { useTheme } from "@mui/material/styles"; // импортировать это
+import {
+  AppBar,
+  Toolbar,
+  Link,
+  Typography,
+  Box,
+  IconButton,
+  Drawer,
+  useMediaQuery,
+} from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useEffect, useState } from "react";
 
 const PrimaryAppBar = () => {
+  const [sideMenu, setSideMenu] = useState(false);
   const theme = useTheme();
+
+  const isSmallScreen = useMediaQuery(theme.breakpoints.up("sm"));
+
+  /* 
+  Тут мы смотрим на размеры экрана и закрвыаем боковое меню, если экран более 600 px. 
+  обрати внимание на завимисость имею ввиде isSmallScreen в последней части выражения
+  оно говорит о том что усвловия будет вклчаться как только сработает isSmallScreem  
+   */  
+  useEffect(()=>{
+    if (isSmallScreen && sideMenu)
+    setSideMenu(false);
+  },[isSmallScreen])
+
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      // Если событие является клавишей Tab или Shift, игнорируем его
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      // В противном случае устанавливаем новое значение sideMenu
+      setSideMenu(open);
+    };
+
   return (
     // в toolbar мы импортируем тему, которую ранее создали
     <AppBar
       sx={{
         /* Ниже мы сбрасываем цвет верхнего меню до белого цвета, а
             также настраиваем цвет нижней границы */
+        zIndex: (theme) => theme.zIndex.drawer + 2,
         backgroundColor: theme.palette.background.default,
         borderBottom: `1px solid ${theme.palette.divider}`,
       }}
@@ -20,11 +61,31 @@ const PrimaryAppBar = () => {
           minHeight: theme.primaryAppBar.height,
         }}
       >
+        <Box sx={{ display: { xs: "block", sm: "none" } }}>
+          <IconButton
+            onClick={toggleDrawer(true)}
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Box>
+
+        <Drawer anchor="left" open={sideMenu} onClose={toggleDrawer(false)}>
+          {[...Array(10)].map((_, i) => (
+            <Typography key={i} paragraph>
+              {i + 1}
+            </Typography>
+          ))}
+        </Drawer>
+
         {/* 
-                Ниже мы настраиваем логотип как ссылку саму на себя
-                Также прописываем размер шрифта, не забудьте импортировать
-                компоненты Link и Typography
-                */}
+          Ниже мы настраиваем логотип как ссылку саму на себя
+          Также прописываем размер шрифта, не забудьте импортировать
+          компоненты Link и Typography
+        */}
         <Link href="/" underline="none" color="inherit">
           <Typography
             variant="h6"
